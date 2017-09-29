12020f6a1e217c4d035cc477c7272cad330ef725
Xin Hu <huxin@pinterest.com>
2017-09-28 17:20:34 -0700

    plex

--- sync_plex.py
+++ sync_plex.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python
 from tendo import singleton
 import os
+import time
 
 me = singleton.SingleInstance()
 
+
+ts = "[{}]".format(time.ctime())
+os.system("echo {} >> /var/log/plex.log".format(ts))
 cmd = "/usr/local/bin/bypy -v --downloader aria2 syncdown plex /root/plex True > /var/log/baidusync/plex.log 2>/var/log/baidusync/plex.err"
 os.system(cmd)
