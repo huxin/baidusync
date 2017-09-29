47d5acc82c08845d1526849734bfc1e5bb47c3fe
Xin Hu <huxin@pinterest.com>
2017-09-28 17:17:22 -0700

    plex

--- sync_plex.py
+++ sync_plex.py
@@ -4,5 +4,5 @@ import os
 
 me = singleton.SingleInstance()
 
-cmd = "/usr/local/bin/bypy -v --downloader aria2 syncdown plex /root/plex True > /var/log/baidusync/plex.lg 2>/var/log/baidusync/plex.err"
+cmd = "/usr/local/bin/bypy -v --downloader aria2 syncdown plex /root/plex True > /var/log/baidusync/plex.log 2>/var/log/baidusync/plex.err"
 os.system(cmd)
