9261f17a37cdf0600a5c9053f0cd9dbc39666d09
Xin Hu <huxin@pinterest.com>
2017-09-28 18:09:57 -0700

    hoxii2

--- sync_hoxii2.py
+++ sync_hoxii2.py
@@ -8,7 +8,7 @@ import time
 me = singleton.SingleInstance()
 
 ts = "[{}]".format(time.ctime())
-os.system("echo {} >> /var/log/hoxii2.log".format(ts))
-os.system("echo {} >> /root/upload_data/hoxii2_upload_ts.txt".format(ts))
+os.system("echo {} >> /var/log/baidusync/hoxii2.log".format(ts))
+os.system("echo {} > /root/upload_data/hoxii2_upload_ts.txt".format(ts))
 cmd = "/usr/local/bin/bypy -e -v syncup /root/upload_data enc >> /var/log/baidusync/hoxii2.log 2>>/var/log/baidusync/hoxii2.err"
 os.system(cmd)
