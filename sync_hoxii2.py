9e223fb031e7eb43a845653b9244c1f92db6a389
Xin Hu <huxin@pinterest.com>
2017-09-28 18:05:51 -0700

    hoxii2

---
+++ sync_hoxii2.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+from tendo import singleton
+import os
+import time
+
+# this should be run every 15 minutes
+
+me = singleton.SingleInstance()
+
+
+ts = "[{}]".format(time.ctime())
+os.system("echo {} >> /var/log/hoxii2.log".format(ts))
+cmd = "/usr/local/bin/bypy -e -v syncup /root/upload_data enc >> /var/log/baidusync/hoxii2.log 2>>/var/log/baidusync/hoxii2.err"
+os.system(cmd)
