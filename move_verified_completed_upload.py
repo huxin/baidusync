ee0d1a6fbea71549912b9d538812ae7a746730f8
Xin Hu <huxin@pinterest.com>
2017-10-29 21:24:29 -0700

    move file

--- move_verified_completed_upload.py
+++ move_verified_completed_upload.py
@@ -7,6 +7,7 @@ if len(sys.argv) != 3:
     exit(1)
 
 # don't delete .encfs6.xml
+base_dir = '/root/upload_data/'
 
 with open(sys.argv[1], 'r') as complete_file:
     for l in complete_file:
@@ -16,4 +17,8 @@ with open(sys.argv[1], 'r') as complete_file:
             print "Error line format:", l
             continue
         equal, md5, remote_path, local_md5, local_path = parts
+        if local_path.startswith(base_dir) == False:
+            print "FATAL, localpath: ", local_path, 'not start with:', base_dir
+            exit(1)
+
         print equal, local_path
