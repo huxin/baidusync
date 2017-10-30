c8bacf4c0bbbe55fc011a0dbce0f8e6f79410446
Xin Hu <huxin@pinterest.com>
2017-10-29 21:21:55 -0700

    move file

--- move_verified_completed_upload.py
+++ move_verified_completed_upload.py
@@ -6,6 +6,14 @@ if len(sys.argv) != 3:
     print "Usage:", sys.argv[0], "complete_res_file destination_dir"
     exit(1)
 
+# don't delete .encfs6.xml
+
 with open(sys.argv[1], 'r') as complete_file:
     for l in complete_file:
-        print l
+        l = l.strip()
+        parts = l.split()
+        if len(parts) > 0 and len(parts) != 4:
+            print "Error line format:", l
+            continue
+        equal, md5, remote_path, local_md5, local_path = parts
+        print equal, local_path
